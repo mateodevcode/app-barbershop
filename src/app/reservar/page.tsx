1a285a1c65bf875f1b30a09e60fed43606fe21f9
Mateo lizcano <mateodevcode@gmail.com>
2025-07-27 20:27:51 +0200

    Se agrego el suspense al searchParams

--- src/app/reservar/page.tsx
+++ src/app/reservar/page.tsx
@@ -1,11 +1,14 @@
 import NavFooter from "@/components/nav-footer/NavFooter";
 import Reservar from "@/components/reservar/Reservar";
 import React from "react";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div className="relative flex flex-col min-h-svh">
-      <Reservar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Reservar />
+      </Suspense>
       <NavFooter />
     </div>
   );
